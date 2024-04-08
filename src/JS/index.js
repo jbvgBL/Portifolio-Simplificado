59864e3e4cdab190d9376b05489606165e88a34d
jbvgBL <166333335+jbvgBL@users.noreply.github.com>
2024-04-07 22:11:03 -0300

    Add files via upload

---
+++ src/JS/index.js
@@ -0,0 +1,18 @@
+const botaoMostarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+
+botaoMostarProjetos.addEventListener('click', () => {
+    mostrarMaisProjetos();
+    esconderBotao();
+});
+
+function esconderBotao() {
+    botaoMostarProjetos.classList.add('remover');
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+}
